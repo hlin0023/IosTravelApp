0a914bcb06e5e7cc34435e31d04d42dab44ebfb6
Huiying Lin <hlin0023@student.monash.edu>
2023-04-25 15:30:28 +1000

    Initial Commit

---
+++ Tripo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tripo
+//
+//  Created by lin on 25/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
